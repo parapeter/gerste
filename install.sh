4a1762c04d3f607a68d5bbc649c49cbc52592114
paranoidpeter <peterparanoid@proton.me>
2024-02-05 20:59:51 +0100

    Added more errorhandling

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 #      Name    : gerste-install
-#      Version : 1.0.0
+#      Version : 1.0.1
 #      License : GNU General Public License v3.0 (https://www.gnu.org/licenses/gpl-3.0)
 #      GitHub  : https://github.com/paranoidpeter/script_name
 #      Author  : paranoidpeter
@@ -24,15 +24,16 @@
 #
 #
 ### GENERAL
-# Error handling
-set -E
+set -o errexit   # abort on nonzero exitstatus
+set -o nounset   # abort on unbound variable
+set -o pipefail  # don't hide errors within pipes
 
 # Version infos
-readonly script_name="gerste-install"
+readonly SCRIPT_NAME="gerste-install"
 
 # Echo helpers
-function error { echo "[ ${script_name} ] error: ${1}"; }
-function info { echo "[ ${script_name} ] info: ${1}"; }
+function error { echo "[ ${SCRIPT_NAME} ] error: ${1}"; }
+function info { echo "[ ${SCRIPT_NAME} ] info: ${1}"; }
 
 ### CHECK FOR ROOT
 [[ ${EUID} != 0 ]] && error "no root" && exit 1
@@ -40,19 +41,13 @@ function info { echo "[ ${script_name} ] info: ${1}"; }
 ### INSTALL OPERATIONS
 # Delete current version
 if [[ -f /usr/local/bin/gerste ]]; then
-    rm -f /usr/local/bin/gerste
-    [[ ${?} -eq 1 ]] && error "could not remove current version.. please delete manually and try again" && exit 1
+    rm --force /usr/local/bin/gerste
 fi
 
 # Check for /usr/local/bin and install
 if [[ -d /usr/local/bin ]]; then
-    
     cp ./gerste.sh /usr/local/bin/gerste &> /dev/null
-    [[ ${?} -eq 1 ]] && error "could not copy to /usr/local/bin.. please copy manually and change permissions" && exit 1
-    
-    chmod 755 /usr/local/bin/gerste &> /dev/null
-    [[ ${?} -eq 1 ]] && error "could not change permissions.. please do it manually for /usr/local/bin/gerste" && exit 1
-
+    chmod 755 /usr/local/bin/gerste &> /dev/null # Change permission to: rwxr-xr-x
 else
     error "/usr/local/bin does not exist" && exit 1
 fi
