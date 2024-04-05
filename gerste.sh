ac665c7e4bfe32e068365aead31cedcb1fe39b56
paranoidpeter <peterparanoid@proton.me>
2024-04-05 13:08:16 +0200

    Added summertime operation and small improvements

--- gerste.sh
+++ gerste.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 #      Name    : gerste (GERman Secure Timesync Execution)
-#      Version : 0.2.5
+#      Version : 0.2.6
 #      License : GNU General Public License v3.0 (https://www.gnu.org/licenses/gpl-3.0)
 #      GitHub  : https://github.com/paranoidpeter/gerste
 #      Author  : paranoidpeter
@@ -29,7 +29,7 @@ set -o nounset   # abort on unbound variable
 set -o pipefail  # don't hide errors within pipes
 
 # Version infos
-readonly VERSION="0.2.5"
+readonly VERSION="0.2.6"
 readonly SCRIPT_NAME="gerste"
 
 # Parameter handling
@@ -80,7 +80,7 @@ fi
 
 
 ### CHECK DEPENDENCIES
-dependencies=( wget date shuf )
+dependencies=( wget date )
 # Check installed packages
 for dependency in "${dependencies[@]}"; do
     [[ -z $(command -v "$dependency") ]] && error "${dependency} is not installed" && exit 1
@@ -89,8 +89,12 @@ debug "dependencies check passed"
 
 
 ### "RANDOM" URL SELECTION
-random_url_selector=$((RANDOM % ${#server_urls[@]})) && readonly random_url_selector
-random_url=${server_urls[${random_url_selector}]} && readonly random_url
+if [[ ${#server_urls[@]} -ne 1 ]]; then
+    random_url_selector=$((RANDOM % ${#server_urls[@]})) && readonly random_url_selector
+    random_url=${server_urls[${random_url_selector}]} && readonly random_url
+else
+    random_url=${server_urls[0]} && readonly random_url
+fi
 
 
 ### GET TIME
@@ -113,7 +117,7 @@ readonly minutes=${time_array[1]}
 readonly seconds=${time_array[2]}
 
 
-### WINTERTIME OPERATIONS
+### SUMMER-/WINTERTIME OPERATIONS
 # Read current timezone and 
 current_timezone=$(date +%Z) && readonly current_timezone
 debug "current_timezone = \"${current_timezone}\""
@@ -122,30 +126,31 @@ debug "current_timezone = \"${current_timezone}\""
 [[ ${current_timezone} == "CET" ]] && readonly wintertime=true || readonly wintertime=false
 
 
-# Increment "hours" if wintertime=true and some formatfixes
+# Increment hours to match CET/CEST-Timezone
 if [[ ${wintertime} == "true" ]]; then
-
-    hours=$((${hours#0} + 1)) # remove leading "0" from hours and increment by 1 (i.e. "08:00:00" -> "9:00:00")
+    hours=$((${hours#0} + 1)) # remove leading "0" from hours and increment by 1 (i.e. "07:00:00" -> "8:00:00")
     debug "wintertime = ${wget_time} -> ${hours}:${minutes}:${seconds}"
+else
+    hours=$((${hours#0} + 2)) # remove leading "0" from hours and increment by 2 (i.e. "07:00:00" -> "9:00:00")
+    debug "summertime = ${wget_time} -> ${hours}:${minutes}:${seconds}"
+fi
 
-    # 24h formatfix (i.e. from "24:56:00" to "00:56:00")
-    if [[ ${hours} -eq 24 ]]; then
-        hours="00"
-        debug " > fixed wintertime format to ${hours}:${minutes}:${seconds}"
-    fi
+# 24h formatfix (i.e. from "24:56:00" to "00:56:00")
+if [[ ${hours} -eq 24 ]]; then
+    hours="00"
+    debug " > fixed timeformat to ${hours}:${minutes}:${seconds}"
+fi
 
-    # re-add leading "0" to hours (i.e. from "9:00:00" to "09:00:00")
-    if [[ ${#hours} -eq 1 ]]; then
-        hours="0${hours}"
-        debug " > fixed wintertime format to ${hours}:${minutes}:${seconds}"
-    fi
-
+# Re-add leading "0" to hours (i.e. from "9:00:00" to "09:00:00")
+if [[ ${#hours} -eq 1 ]]; then
+    hours="0${hours}"
+    debug " > fixed timeformat to ${hours}:${minutes}:${seconds}"
 fi
 
 
 ### CHECK IF new_time DIFFERS TO current_time
 new_time="${hours}:${minutes}:${seconds}" && readonly new_time
-[[ ${new_time} == "$current_time" ]] && info "nothing to do here, time is correct already" && exit 0
+[[ ${new_time} == "$current_time" ]] && info "nothing to do here.. time is correct already" && exit 0
 
 ### VALIDATE DATE FORMAT
 date --date "${hours}:${minutes}:${seconds}" &> /dev/null
