ecdc28a06926d3a85fd232d45bc6672cfd38c79d
paranoidpeter <peterparanoid@proton.me>
2024-11-13 22:45:37 +0100

    Rewrite to function based

--- gerste.sh
+++ gerste.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
-#      Name    : gerste (GERman Secure Time Editor)
-#      Version : 0.3.0
+#      Name    : gerste (GERman-Secure-Time-Editor)
+#      Version : 0.4.0
 #      License : GNU General Public License v3.0 (https://www.gnu.org/licenses/gpl-3.0)
 #      GitHub  : https://github.com/parapeter/gerste
 #      Author  : parapeter
@@ -28,174 +28,202 @@ set -o errexit   # abort on nonzero exitstatus
 set -o nounset   # abort on unbound variable
 set -o pipefail  # don't hide errors within pipes
 
-# Versioninfos
-readonly VERSION="0.3.0"
+readonly VERSION="0.4.0"
 readonly SCRIPT_NAME="gerste"
+readonly CONF_PATH="/etc/gerste.conf"
 
-# Check root
-if [[ ${EUID} -ne 0 ]]; then
-    echo "[ ${SCRIPT_NAME} ] error: root permissions needed"
-    exit 1
-fi
+### ECHO & LOGGING HELPERS
+function info() {
+    local message="${1:-"Unknown error"}"
+    echo "[ ${SCRIPT_NAME} ] info: ${message}"
+    logger "[ ${SCRIPT_NAME} ] info: ${message}"
+}
 
-# Printhelpers
-function error {
-    echo "[ ${SCRIPT_NAME} ] error: ${1}"
-    logger -p user.err "[ ${SCRIPT_NAME} ] error: ${1}"
+function error() {
+    local message="${1:-"Unknown error"}"
+    echo "[ ${SCRIPT_NAME} ] error: ${message}"
+    logger -p user.err "[ ${SCRIPT_NAME} ] error: ${message}"
     exit 1
 }
 
-function info {
-    echo "[ ${SCRIPT_NAME} ] info: ${1}"
-    logger "[ ${SCRIPT_NAME} ] info: ${1}"
+function check_root() {
+    if [[ ${EUID} -ne 0 ]]; then
+        error "you need root access"
+    fi
 }
 
-# Parameter handling
+### PARAMETER HANDLING
 tor_enabled=false
-for parameter in "$@"; do
-    case "$parameter" in
-        -t|--tor)
-            tor_enabled=true
-            ;;
-        -v|--version)
-            echo "${SCRIPT_NAME}-${VERSION}"
-            exit 0
-            ;;
-        *)
-            error "illegal parameter: ${parameter}"
-            ;;
-    esac
-done
-
-### DEPENDENCIES AND URLs
-# Check if needed dependencies are installed
-dependencies=( wget date grep )
-for dependency in "${dependencies[@]}"; do
-    [[ -z $(command -v "$dependency") ]] && error "${dependency} is not installed"
-done
-
-# Get wget execution path
-WGET_PATH=$(which wget) && readonly WGET_PATH
-
-# Set URLs 
-# NOTE: Test new URLs before adding to server_urls; Expected output format: 12:34:56
-#   > wget -S --spider -t 1 --timeout=10 --max-redirect=0 --no-cookies --delete-after "example-url.org" 2>&1 | grep -i "Date:" | grep -o "[0-2][0-9]:[0-5][0-9]:[0-5][0-9]""
-if [[ "$tor_enabled" == "true" ]]; then
- 
-    # Check if tor is active and torsocks is installed
-    if [[ -z $(pgrep -x tor) ]]; then
-        error "no tor process found"
-    fi
-    if [[ -z $(command -v torsocks) ]]; then
-        echo "torsocks is not installed"
-    fi
-
-    # Load TOR URLs from /etc/gerste.conf
-    readarray -t server_urls < <(awk '/^\[TOR-URLS\]/{found=1; next} /^\[/{found=0} found && !/^#/ && NF{print}' /etc/gerste.conf)
-
-    for url in "${server_urls[@]}"; do
-        if ! [[ $url =~ \.onion$ ]]; then
-            error "invalid onion URL format: ${url}. URL should end with \".onion\""
-        fi
+function parse_parameters() {
+    for parameter in "$@"; do
+        case "$parameter" in
+            -t|--tor)
+                tor_enabled=true
+                ;;
+            -v|--version)
+                echo "${SCRIPT_NAME}-${VERSION}"
+                exit 0
+                ;;
+            *)
+                error "invalid parameter: ${parameter}"
+                ;;
+        esac
     done
+}
 
-else
-    # Load clearweb URLs from /etc/gerste.conf
-    readarray -t server_urls < <(awk '/^\[CLEARWEB-URLS\]/{found=1; next} /^\[/{found=0} found && !/^#/ && NF{print}' /etc/gerste.conf)
-fi
+### DEPENDENCIES
+function check_dependencies() {
+    if [[ ! -f "$CONF_PATH" ]]; then
+        error "could not find $CONF_PATH"
+    fi
 
-### "RANDOM" URL SELECTION
-if [[ ${#server_urls[@]} -lt 1 ]]; then
-    error "no URLs given"
-elif [[ ${#server_urls[@]} -eq 1 ]]; then
-    random_url=${server_urls[0]} && readonly random_url
-else
-    random_url_selector=$((RANDOM % ${#server_urls[@]})) && readonly random_url_selector
-    random_url=${server_urls[${random_url_selector}]} && readonly random_url
-fi
+    if [[ "$tor_enabled" == "true" ]]; then
+        if [[ -z $(pgrep -x tor) ]]; then
+            error "no tor process found"
+        fi
+        local dependencies=( wget date grep torsocks )
+    else
+        local dependencies=( wget date grep )
+    fi
 
-### TIME EXTRACTION
-if [[ "$tor_enabled" == "true" ]]; then
-    # Get new time via wget (WITH torsocks) + recheck if execution was successful
-    fetched_time=$(torsocks "$WGET_PATH" --server-response --spider -t 1 --timeout=10 --max-redirect=0 --no-cookies --no-cache --delete-after "$random_url" 2>&1 | grep -i "Date:" | grep -o "[0-2][0-9]:[0-5][0-9]:[0-5][0-9]") && readonly fetched_time
-else
-    # Get new time via wget (WITHOUT torsocks) + recheck if execution was successful
-    fetched_time=$("$WGET_PATH" -S --spider -t 1 --timeout=10 --max-redirect=0 --no-cookies --delete-after "$random_url" 2>&1 | grep -i "Date:" | grep -o "[0-2][0-9]:[0-5][0-9]:[0-5][0-9]") && readonly fetched_time
-fi
+    for dependency in "${dependencies[@]}"; do
+        check_command "$dependency"
+    done
+}
 
-# Check if fetched_time is filled before further processing
-if [[ -z "$fetched_time" ]]; then
-    error "could not resolve URL: ${random_url}"
-fi
+function check_command() {
+    local command_name="${1}"
+    if ! command -v "$command_name" &> /dev/null; then
+        error "${command_name} needs to be installed"
+    fi
+}
 
-# Get current systemtime to compare later
-current_time=$(date +%H:%M:%S) && readonly current_time
+### PREPARE URL
+# NOTE: Test new URLs before adding to /etc/gerste.conf -> Expected output format: 12:34:56
+#   $ wget -S --spider -t 1 --timeout=10 --max-redirect=0 --no-cookies --delete-after "example-url.org" 2>&1 | grep -i "Date:" | grep -o "[0-2][0-9]:[0-5][0-9]:[0-5][0-9]""
+function load_urls() {
+    if [[ "$tor_enabled" == "true" ]]; then
+        readarray -t urls < <(awk '/^\[TOR-URLS\]/{found=1; next} /^\[/{found=0} found && !/^#/ && NF{print}' "$CONF_PATH")
+    else
+        readarray -t urls < <(awk '/^\[HTTPS-URLS\]/{found=1; next} /^\[/{found=0} found && !/^#/ && NF{print}' "$CONF_PATH")
+    fi
+    
+    echo "${urls[@]}"
+}
 
-# Split fetched_time into hours, minutes and seconds
-if [[ $(echo "$fetched_time" | grep -o ":" | wc -l) -ne 2 ]]; then
-    error "fetched time format is invalid: ${fetched_time}"
-else
-    IFS=':' read -ra time_array <<< "$fetched_time"
-    hours=${time_array[0]}
-    readonly minutes=${time_array[1]}
-    readonly seconds=${time_array[2]}
-fi
+function validate_onion_url() {
+    local url="${1:-"Error"}"
 
-### SUMMER-/WINTERTIME OPERATIONS
-# Read current timezone
-current_timezone=$(date +%Z) && readonly current_timezone
+    if [[ ! "$url" =~ \.onion$ ]]; then
+        error "invalid onion URL (must end with .onion): ${url}"
+    fi
+}
 
-# Check if timezone matches CET (wintertime)
-if [[ ${current_timezone} == "CET" ]]; then
-    readonly wintertime=true
-else
-    readonly wintertime=false
-fi
+function validate_https_url() {
+    local url="${1:-"Error"}"
 
-# Increment hours to match CET/CEST-Timezone
-if [[ ${wintertime} == "true" ]]; then
-    hours=$((${hours#0} + 1)) # remove leading "0" from hours if present and increment by 1 for wintertime(i.e. "07:00:00" -> "8:00:00")
-else
-    hours=$((${hours#0} + 2)) # remove leading "0" from hours if present and increment by 2 for summertime(i.e. "07:00:00" -> "9:00:00")
-fi
+    if [[ ! "$url" =~ ^https:// ]]; then
+        error "invalid URL (must start with https://): ${url}"
+    fi
+}
 
-# 24h formatfix (i.e. from "24:56:00" to "00:56:00")
-if [[ ${hours} -eq 24 ]]; then
-    hours="00"
-fi
+function select_random_url() {
+    local -a urls=("$@")
 
-# Re-add leading "0" to hours if missing (i.e. from "9:00:00" to "09:00:00")
-if [[ ${#hours} -eq 1 ]]; then
-    hours="0${hours}"
-fi
-readonly hours
+    if [[ ${#urls[@]} -eq 1 ]]; then
+        echo "${urls[0]}"
+    else
+        local random_index=$((RANDOM % ${#urls[@]}))
+        echo "${urls[${random_index}]}"
+    fi
+}
 
-### VALIDATE DATE FORMAT
-# Check if values are valid and in range
-if ! [[ ${hours} =~ ^[0-2][0-9]$ && ${minutes} =~ ^[0-5][0-9]$ && ${seconds} =~ ^[0-5][0-9]$ ]]; then
-    error "fetched time is not valid: ${hours}:${minutes}:${seconds}"
-fi
+### TIME OPERATIONS
+function fetch_time() {
+    local url="${1:-"Error"}"
 
-if [[ "$hours" > "24" ]]; then
-    error "fetched time is not valid: ${hours}:${minutes}:${seconds}"
-fi
+    # Fetch HTTP header response
+    if [[ "$tor_enabled" == "true" ]]; then
+        if ! response=$(torsocks wget -S --spider -t 1 --timeout=10 --max-redirect=0 --no-cookies --delete-after "$url" 2>&1); then
+            error "could not fetch tor URL: $url"
+        fi
+    else
+        if ! response=$(wget -S --spider -t 1 --timeout=10 --max-redirect=0 --no-cookies --delete-after "$url" 2>&1); then
+            error "could not fetch URL: $url"
+        fi
+    fi
 
-# Set new_time
-readonly new_time="${hours}:${minutes}:${seconds}"
+    # Extract time from response
+    if ! date_info=$(echo "$response" | grep -i "Date:" | grep -o "[0-2][0-9]:[0-5][0-9]:[0-5][0-9]"); then
+        error "could not extract time from server response"
+    fi
 
-# Recheck with date command
-if ! date --date "$new_time" &> /dev/null; then
-    error "broken timeformat: ${new_time}"
-fi
+    echo "$date_info"
+}
 
-### TIME SETTING
-# Check if systemtime differs to fetched time
-if [[ ${new_time} == "$current_time" ]]; then
-    info "nothing to do here.. time is correct already"
-    exit 0
-else
-    # Set new systemtime
-    date --set "$new_time" &> /dev/null
-    info "systemtime updated to ${new_time}"
-    exit 0
-fi
+function adjust_for_timezone() {
+    local time="${1:-"Error"}"
+    local timezone
+    timezone=$(date +%Z)
+
+    IFS=':' read -r hours minutes seconds <<< "$time" # Extract hh:mm:ss into variables
+
+    # Add hours to match CET/CEST-Timezone
+    if [[ "$timezone" == "CET" ]]; then
+        hours=$((${hours#0} + 1))
+    else
+        hours=$((${hours#0} + 2))
+    fi
+
+    [[ $hours -eq 24 ]] && hours="00" # Format-Fix (i.e. 24:54:00 -> 00:54:00)
+    [[ ${#hours} -eq 1 ]] && hours="0${hours}" # Format-Fix (i.e. 8:00:00 -> 08:00:00)
+
+    echo "${hours}:${minutes}:${seconds}"
+}
+
+function set_system_time() {
+    local new_time="${1:-"Error"}"
+    local current_time
+    current_time=$(date +%H:%M:%S)
+
+    # Validate new_time
+    if ! date --date "$new_time" &> /dev/null; then
+        error "broken timeformat: ${new_time}"
+    fi
+
+    # Set new time if differs to current systemtime
+    if [[ "$new_time" == "$current_time" ]]; then
+        info "nothing to do here.. time is correct already."
+    else
+        date --set "$new_time" &> /dev/null && info "set systemtime to: ${new_time}"
+    fi
+}
+
+### MAIN
+function main() {
+    check_root
+
+    parse_parameters "$@"
+    check_dependencies
+
+    local -a server_urls=()
+    while IFS='' read -r url; do server_urls+=("$url"); done < <(load_urls) # https://www.shellcheck.net/wiki/SC2207
+
+    local random_url
+    random_url=$(select_random_url "${server_urls[@]}")
+
+    if [[ "$tor_enabled" == "true" ]]; then
+        validate_onion_url "$random_url"
+    else
+        validate_https_url "$random_url"
+    fi
+
+    local fetched_time
+    fetched_time=$(fetch_time "$random_url")
+
+    local adjusted_time
+    adjusted_time=$(adjust_for_timezone "$fetched_time")
+
+    set_system_time "$adjusted_time"
+}
+main "$@"
