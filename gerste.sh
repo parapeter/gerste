5f089c0710f4afa2884fcba1588db119e94d8141
paranoidpeter <peterparanoid@proton.me>
2024-12-22 19:03:15 +0100

    Add support for all timezones

--- gerste.sh
+++ gerste.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 #      Name    : gerste (GERman-Secure-Time-Editor)
-#      Version : 0.5.0
+#      Version : 0.5.1
 #      License : GNU General Public License v3.0 (https://www.gnu.org/licenses/gpl-3.0)
 #      GitHub  : https://github.com/parapeter/gerste
 #      Author  : parapeter <parapeter-git@proton.me>
@@ -27,7 +27,7 @@ set -o errexit   # abort on nonzero exitstatus
 set -o nounset   # abort on unbound variable
 set -o pipefail  # don't hide errors within pipes
 
-readonly VERSION="0.5.0"
+readonly VERSION="0.5.1"
 readonly SCRIPT_NAME="gerste"
 readonly CONF_PATH="/etc/gerste.conf"
 
@@ -35,8 +35,16 @@ tor_enabled=false
 dry_run_enabled=false
 
 ### HELPERS ###
+function check_parameter_error() {
+    local parameter="${1:-"ERROR"}"
+    if [[ "$parameter" == "ERROR" ]]; then
+        error "an error accured while passing parameters"
+    fi
+}
+
 function print_info() {
     local message="${1:-"ERROR"}"
+    check_parameter_error "$message"
     echo "[ ${SCRIPT_NAME} ] info: ${message}"
     logger "[ ${SCRIPT_NAME} ] info: ${message}"
 }
@@ -46,13 +54,6 @@ function error() {
     echo "[ ${SCRIPT_NAME} ] error: ${message}"
     logger -p user.err "[ ${SCRIPT_NAME} ] error: ${message}"
     exit 1
-}
-
-function check_parameter_error() {
-    local parameter="${1:-"ERROR"}"
-    if [[ "$parameter" == "ERROR" ]]; then
-        error "an error accured while passing parameters"
-    fi
 }
 
 function random_from_array() {
@@ -95,20 +96,31 @@ function parse_parameters() {
 
 ### DEPENDENCIES ###
 function check_dependencies() {
+    # Check if dependencies are installed
     local dependencies=( curl date grep )
+    for dependency in "${dependencies[@]}"; do
+        is_command_installed "$dependency"
+    done
 
+    # Check if config file is present
     if [[ ! -f "$CONF_PATH" ]]; then
         error "could not find $CONF_PATH"
+    fi
+
+    # Check if /etc/localtime is set
+    if [[ ! -e /etc/localtime ]]; then
+        error "could not find /etc/localtime"
+    fi
+
+    # Check if ca-certificates are present
+    if [[ ! -f /etc/ssl/certs/ca-certificates.crt ]]; then
+        error "could not find /etc/ssl/certs/ca-certificates.crt"
     fi
 
     # Check if tor is running (option -t/--tor)
     if [[ "$tor_enabled" == "true" && -z $(pgrep -x tor) ]]; then
         error "no tor process found"
     fi
-
-    for dependency in "${dependencies[@]}"; do
-        is_command_installed "$dependency"
-    done
 }
 
 function is_command_installed() {
@@ -196,32 +208,71 @@ function fetch_time() {
     echo "$date_info"
 }
 
-function adjust_for_timezone() {
-    local time="${1:-"ERROR"}"
-    local timezone
-    timezone=$(date +%Z) || error "could not get current timezone"
-    check_parameter_error "$time"
+function adjust_timezone_offset() {
+    local fetched_time="${1:-"ERROR"}"
+    check_parameter_error "$fetched_time"
+    
+    local offset
+    offset=$(date +%z) || error "could not detect timezone offset"
 
-    IFS=':' read -r hours minutes seconds <<< "$time" # Extract hh:mm:ss into variables
-
-    # Add hours to match CET/CEST-Timezone
-    if [[ "$timezone" == "CET" ]]; then
-        hours=$((${hours#0} + 1)) # Remove leading 0 and add 1
+    if [[ $offset =~ ^([+-])([0-9]{2})([0-9]{2})$ ]]; then
+        local offset_operator="${BASH_REMATCH[1]}" # Extract "+" or "-" if regex matches
+        local offset_hours="${BASH_REMATCH[2]}" # Extract hours offset if regex matches
+        local offset_minutes="${BASH_REMATCH[3]}" # Extract minutes offset if regex matches
     else
-        hours=$((${hours#0} + 2)) # Remove leading 0 and add 2
+        error "unknown timezone offset format"
     fi
 
-    [[ $hours -eq 24 ]] && hours="00" # Format-Fix (i.e. 24:54:00 -> 00:54:00)
-    [[ ${#hours} -eq 1 ]] && hours="0${hours}" # Format-Fix (i.e. 8:00:00 -> 08:00:00)
+    local hours
+    local minutes
+    local seconds
+    IFS=':' read -r hours minutes seconds <<< "$fetched_time" # Extract hh:mm:ss into variables
+
+    # Add hours to match current system timezone
+    if [[ "$offset_operator" == "+" ]]; then
+        hours=$((${hours#0} + ${offset_hours#0})) # Remove leading 0's and add hours offset
+        minutes=$((${minutes#0} + ${offset_minutes#0})) # Remove leading 0's and add minutes offset
+    elif [[ "$offset_operator" == "-" ]]; then
+        hours=$((${hours#0} - ${offset_hours#0})) # Remove leading 0 and subtract hours offset
+        minutes=$((${minutes#0} - ${offset_minutes#0})) # Remove leading 0 and subtract minutes offset
+    else
+        error "unknown offset operator"
+    fi
+
+    # Format-Fix for $minutes greater than (or equal to) 60, i.e. 05:84:00 -> 06:24:00
+    if [[ $minutes -ge 60 ]]; then
+        hours=$((hours + 1))
+        minutes=$((minutes % 60))
+    fi
+
+    # Format-Fix for $hours greater than (or equal to) 24, i.e. 26:54:00 -> 02:54:00 (or 24:54:00 -> 00:54:00)
+    if [[ $hours -ge 24 ]]; then
+        hours=$((hours % 24))
+    fi
+
+    # Format-Fix for $minutes less than 0, i.e. 04:-20:00 -> 03:40:00
+    if [[ $minutes -lt 0 ]]; then
+        hours=$((hours - 1))
+        minutes=$((minutes + 60))
+    fi
+
+    # Format-Fix for $hours less than 0, i.e. -2:54:00 -> 22:54:00
+    if [[ $hours -lt 0 ]]; then
+        hours=$((hours + 24))
+    fi
+
+    # Additional Format-Fixes, i.e. 8:00:00 -> 08:00:00 (or 08:4:00 -> 08:04:00)
+    [[ ${#hours} -eq 1 ]] && hours="0${hours}"
+    [[ ${#minutes} -eq 1 ]] && minutes="0${minutes}"
 
     echo "${hours}:${minutes}:${seconds}"
 }
 
 function set_system_time() {
     local new_time="${1:-"ERROR"}"
+    check_parameter_error "$new_time"
     local current_time
     current_time=$(date +%H:%M:%S) || error "could not get current time"
-    check_parameter_error "$new_time"
 
     # Validate new_time with date
     date --date "$new_time" &> /dev/null || error "broken timeformat: ${new_time}"
@@ -256,7 +307,7 @@ function main() {
 
     # Adjust time
     local adjusted_time
-    adjusted_time=$(adjust_for_timezone "$fetched_time")
+    adjusted_time=$(adjust_timezone_offset "$fetched_time")
 
     # Set time (or print in dry-run)
     if [[ "$dry_run_enabled" == "true" ]]; then
